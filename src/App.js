236a9722ca275a7071244fb0fa6cf8f35419de5e
Ziyoviddin <ziyoviddinabdurashidov7@gmail.com>
2023-10-05 21:46:23 +0500

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
 export default function App() {
   return (
     <div className="container">
-      <FontAwesomeIcon icon={faUser} />
+      {/* <FontAwesomeIcon icon={faUser} /> */}
       <Navbar />
 
       <Footer />
