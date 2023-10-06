98425e64fc98ff63067b9e6e7a0a4b2f352c6481
Ziyoviddin <ziyoviddinabdurashidov7@gmail.com>
2023-10-06 10:48:30 +0500

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
 export default function App() {
   return (
-    <div className="container">
-      {/* <FontAwesomeIcon icon={faUser} /> */}
-      <Navbar />
-
+    <div>
+      <div className="container">{/* <Navbar /> */}</div>
       <Footer />
     </div>
   );
