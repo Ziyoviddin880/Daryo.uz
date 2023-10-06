005d1a709bb810cc2238368aa496ab47c39b7b88
Ziyoviddin <ziyoviddinabdurashidov7@gmail.com>
2023-10-06 19:39:33 +0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./global.scss";
-
-const htmlWrite = document.getElementById("root");
-const root = createRoot(htmlWrite);
-root.render(<App />);
